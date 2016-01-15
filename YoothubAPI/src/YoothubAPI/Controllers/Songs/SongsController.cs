28fe92bf558241a58e80f1096a0dbb645ea94c2b
Marcin <mwazelinski@gmail.com>
2016-01-15 16:13:06 +0100

    upvote downvote

---
+++ YoothubAPI/src/YoothubAPI/Controllers/Songs/SongsController.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoothubAPI.Models;
+using System.Security.Claims;
+using YoothubAPI.Services;
+using Microsoft.QueryStringDotNET;
+using Swashbuckle.SwaggerGen.Annotations;
+using System.Net;
+using Microsoft.Data.Entity;
+
+namespace YoothubAPI.Controllers.Songs
+{
+    [Route("api/[controller]")]
+    public class SongsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        private readonly IYoutubeService _youtubeService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public SongsController(IYoutubeService youtubeService, UserManager<ApplicationUser> userManager)
+        {
+            _youtubeService = youtubeService;
+            _userManager = userManager;
+        }
+
+        // GET: api/songs?page=5&pageSize=20
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(SongsJson))]
+        public IActionResult Get([FromQuery]int? page, [FromQuery]int? pageSize = 20)
+        {
+            if (page < 0 || pageSize < 0) return new BadRequestResult();
+
+            var count = db.Songs.Count();
+
+            if (!page.HasValue)
+                return Json(new SongsJson
+                {
+                    Count = count,
+                    Results = db.Songs
+                        .Include(s => s.AddedBy)
+                        .Include(s => s.SongTags)
+                        .ThenInclude(st => st.Tag)
+                });
+
+            return Json(new SongsJson
+            {
+                Count = count,
+                Results = db.Songs
+                    .Include(s => s.AddedBy)
+                    .Include(st => st.SongTags)
+                    .Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value)
+            });
+        }
+
+        // GET api/songs/5
+        [HttpGet("{id}")]
+        public async Task<Song> Get(int id)
+        {
+            return await db.Songs
+                .Include(s => s.AddedBy)
+                .Include(st => st.SongTags)
+                .FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        // POST api/songs
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Post([FromBody]AddSongJson value)
+        {
+            var query = new Uri(value.URL).Query.TrimStart('?');
+
+            var ytInfo = await _youtubeService.GetYoutubeInfo(QueryString.Parse(query)["v"]);
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+
+            var song = new Song
+            {
+                Added = DateTime.Now,
+                AddedBy = currentUser,
+                Broken = false,
+                Duration = ytInfo.Duration,
+                LastPlayed = DateTime.MinValue,
+                Title = ytInfo.Title,
+                URL = value.URL,
+                Votes = 0
+            };
+
+            foreach(var tag in value.Tags)
+            {
+                var dbTag = db.Tags.FirstOrDefault(t => t.Name == tag);
+                if(dbTag == null)
+                {
+                    dbTag = new Tag
+                    {
+                        Name = tag
+                    };
+                    db.Tags.Add(dbTag);
+                }
+
+                db.SongTags.Add(
+                    new SongTag()
+                    {
+                        Song = song,
+                        Tag = dbTag
+                    });
+            }
+
+            db.Add(song);
+            db.SaveChanges();
+
+            return new EmptyResult();
+        }
+
+        // DELETE api/songs/5
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var song = await this.Get(id);
+
+            if (song.AddedBy.Id != User.GetUserId())
+                return new HttpStatusCodeResult(403);
+
+            db.Remove(song);
+            db.SaveChanges();
+            return new EmptyResult();
+        }
+
+        [HttpPost("Upvote/{id}")]
+        [Authorize]
+        public async Task<IActionResult> Upvote(int id)
+        {
+            var song = await this.Get(id);
+            if (song == null) return new BadRequestObjectResult("Song with given id doesn't exist.");
+            var result = await VoteSong(song, VoteType.Upvote);
+            return result;
+        }
+
+        [HttpPost("Downvote/{id}")]
+        [Authorize]
+        public async Task<IActionResult> Downvote(int id)
+        {
+            var song = await this.Get(id);
+            if (song == null) return new BadRequestObjectResult("Song with given id doesn't exist.");
+            var result = await VoteSong(song, VoteType.Downvote);
+            return result;
+        }
+
+        private async Task<IActionResult> VoteSong(Song song, VoteType voteType)
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+
+            var vote = db.Votes.FirstOrDefault(v => v.Song.Id == song.Id && v.User.Id == currentUser.Id);
+            if(vote != null)
+            {
+                if (vote.VoteType == voteType)
+                    return new ObjectResult("User already voted on that song.") { StatusCode = 406 };
+                else
+                {
+                    vote.VoteType = voteType;
+                    switch (voteType)
+                    {
+                        case VoteType.Upvote:
+                            song.Votes++;
+                            break;
+
+                        case VoteType.Downvote:
+                            song.Votes--;
+                            break;
+                    }
+                }
+                    
+            }
+            else
+            {
+                vote = new Vote()
+                {
+                    Song = song,
+                    User = currentUser,
+                    VoteType = voteType
+                };
+
+                db.Votes.Add(vote);
+            }
+
+            switch (voteType)
+            {
+                case VoteType.Upvote:
+                    song.Votes++;
+                    break;
+
+                case VoteType.Downvote :
+                    song.Votes--;
+                    break;
+            }
+
+            db.SaveChanges();
+            return new EmptyResult();
+        }
+    }
+}
