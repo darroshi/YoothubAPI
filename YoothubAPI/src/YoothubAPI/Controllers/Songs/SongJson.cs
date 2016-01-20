f4802dea9cd07dc57f7f09390e62bac9356552af
Marcin Ważeliński <mwazelinski@gmail.com>
2016-01-20 15:20:52 +0100

    serialize votetype as string

--- YoothubAPI/src/YoothubAPI/Controllers/Songs/SongJson.cs
+++ YoothubAPI/src/YoothubAPI/Controllers/Songs/SongJson.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +12,7 @@ namespace YoothubAPI.Controllers.Songs
     {
         public Song Song { get; set; }
 
+        [JsonConverter(typeof(StringEnumConverter))]
         public VoteType? CurrentVote { get; set; }
     }
 }
