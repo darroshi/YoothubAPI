9c7631cca4e76ef23f6d530ec70f40cb4ad8b783
Marcin <mwazelinski@gmail.com>
2016-02-06 22:36:43 +0100

    closes #6, breaking change GET /api/Play

---
+++ YoothubAPI/src/YoothubAPI/Controllers/Play/CurrentSongJson.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoothubAPI.Models;
+
+namespace YoothubAPI.Controllers.Play
+{
+    public class CurrentSongJson
+    {
+        public Song Song { get; set; }
+
+        [JsonConverter(typeof(StringEnumConverter))]
+        public VoteType? CurrentVote { get; set; }
+    }
+}
--- YoothubAPI/src/YoothubAPI/Controllers/Play/PlayController.cs
+++ YoothubAPI/src/YoothubAPI/Controllers/Play/PlayController.cs
@@ -1,5 +1,12 @@
 ﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Swashbuckle.SwaggerGen.Annotations;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using YoothubAPI.Models;
 using YoothubAPI.Services;
 
 namespace YoothubAPI.Controllers.Play
@@ -9,18 +16,30 @@ namespace YoothubAPI.Controllers.Play
     public class PlayController : Controller
     {
         private readonly IPlaybackService _playbackService;
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public PlayController(IPlaybackService playbackService)
+        public PlayController(IPlaybackService playbackService, ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager)
         {
             _playbackService = playbackService;
+            _db = dbContext;
+            _userManager = userManager;
         }
 
         [HttpGet]
         [AllowAnonymous]
-        public IActionResult Get()
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(CurrentSongJson))]
+        public async Task<IActionResult> Get()
         {
-            var currentSong = _playbackService.GetCurrentSong();
-            return Json(currentSong);
+            var currentSong = await Task.Run(() => _playbackService.GetCurrentSong());
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            var currentUserId = currentUser?.Id;
+
+            var vote = await _db.Votes
+                .Include(v => v.User)
+                .FirstOrDefaultAsync(v => v.Id == currentSong.Id && v.User.Id == currentUserId);
+
+            return Json(new CurrentSongJson { Song = currentSong, CurrentVote = vote != null ? (VoteType?)vote.VoteType : null });
         }
     }
 }
