50b3e3879d165a04dd5997805aeac5a377d5daca
Marcin <mwazelinski@gmail.com>
2016-04-03 14:38:48 +0200

    test fail testing

--- YoothubAPI/src/YoothubAPI.Tests/SongsControllerTests.cs
+++ YoothubAPI/src/YoothubAPI.Tests/SongsControllerTests.cs
@@ -67,6 +67,7 @@ namespace YoothubAPI.Tests
         {
             using (var controller = new SongsController(_youtubeService, _userManager, _loggerFactory, _dbContext))
             {
+                throw new Exception();
                 controller.ActionContext = new ActionContext { HttpContext = _contextMock };
                 var result = await controller.Get(null, null);
                 Assert.IsType<JsonResult>(result);
